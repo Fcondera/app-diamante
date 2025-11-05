1a06b8c912344cc38ffbcdc822bbc2f7ec49899e
asdasd <erfef@gmail.com>
2025-11-04 21:13:08 -0300

    fix: remover backend S3 para usar backend local

--- modulo7-iac_tooling/main.tf
+++ modulo7-iac_tooling/main.tf
@@ -5,14 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-  
-  backend "s3" {
-    bucket         = "jewelry-app-terraform-state"
-    key            = "jewelry-app/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "jewelry-app-terraform-locks"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
